3a6605a7f999f59d11c4ffd16d1a080c6d5c9b74
Landon Borrego <lndnborrrego@gmail.com>
2025-08-23 14:05:10 -0400

    Create view for workout complete and runs successfully.

--- simplefitapi/views/workout_views.py
+++ simplefitapi/views/workout_views.py
@@ -1,7 +1,7 @@
-from rest_framework import serializers
+from rest_framework import serializers, status
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
-from simplefitapi.models import Workout
+from simplefitapi.models import Workout, User, MuscleGroup
 
 class WorkoutView(ViewSet):
 
@@ -20,6 +20,26 @@ class WorkoutView(ViewSet):
 
         serialized = WorkoutSerializer(workouts, many=True)
         return Response(serialized.data)
+
+    def create(self, request):
+        """create view for workouts"""
+
+        user = User.objects.get(pk = request.data['user_id'])
+        muscle_group = MuscleGroup.objects.get(pk = request.data['muscle_group_id'])
+
+        workout = Workout.objects.create(
+            name = request.data['name'],
+            num_of_sets = request.data['num_of_sets'],
+            total_reps = request.data['total_reps'],
+            max_weight = request.data['max_weight'],
+            time_stamp = request.data['time_stamp'],
+            is_complete = request.data['is_complete'],
+            muscle_group_id = muscle_group,
+            user_id = user
+        )
+
+        serialized = WorkoutSerializer(workout)
+        return Response(serialized.data, status=status.HTTP_201_CREATED)
 
 class WorkoutSerializer(serializers.ModelSerializer):
     """ JSON serializer for workouts """
